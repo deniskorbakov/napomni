1615ac5a3b3503341a739ccc8e2441e8d19ec567
deniskorbakov <korbakovd@gmail.com>
2023-03-21 20:23:34 +0300

    добавил футер

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,114 @@
+<!-- Footer -->
+<footer class="text-center text-lg-start">
+    <!-- Section: Social media -->
+    <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+        <!-- Left -->
+        <div class="me-5 d-none d-lg-block">
+            <span>Найти меня в социальных сетях:</span>
+        </div>
+        <!-- Left -->
+
+        <!-- Right -->
+        <div>
+            <a href="https://www.linkedin.com/in/deniskorbakov/?trk=public-profile-join-page" class="me-4 text-reset" target="_blank" style="text-decoration: none;">
+                <i class="fa-brands fa-linkedin fa-xl"></i>
+            </a>
+
+            <a href="https://github.com/deniskorbakov" class="me-4 text-reset" target="_blank" style="text-decoration: none;">
+                <i class="fa-brands fa-github fa-xl"></i>
+            </a>
+
+            <a href="https://vk.com/deniskorbakov" class="me-4 text-reset" target="_blank" style="text-decoration: none;">
+                <i class="fa-brands fa-vk fa-xl"></i>
+            </a>
+        </div>
+        <!-- Right -->
+    </section>
+    <!-- Section: Social media -->
+
+    <!-- Section: Links  -->
+    <section>
+        <div class="container text-center text-md-start mt-5">
+            <!-- Grid row -->
+            <div class="row mt-3">
+                <!-- Grid column -->
+                <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                    <!-- Content -->
+                    <h6 class="text-uppercase fw-bold mb-4">
+                        <img src="icon/logo.svg" alt="logo" width="50" height="20">Reminder
+                    </h6>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias asperiores at atque dolores doloribus esse incidunt laboriosam minima nam nemo officia officiis omnis sapiente sunt totam, vitae voluptatem! Doloribus, voluptas.
+                    </p>
+                </div>
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+                <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+                    <!-- Links -->
+                    <h6 class="text-uppercase fw-bold mb-4">
+                        Products
+                    </h6>
+                    <p>
+                        <a href="#!" class="text-reset">Angular</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-reset">React</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-reset">Vue</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-reset">Laravel</a>
+                    </p>
+                </div>
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+                <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+                    <!-- Links -->
+                    <h6 class="text-uppercase fw-bold mb-4">
+                        Useful links
+                    </h6>
+                    <p>
+                        <a href="#!" class="text-reset">Pricing</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-reset">Settings</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-reset">Orders</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-reset">Help</a>
+                    </p>
+                </div>
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+                <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+                    <!-- Links -->
+                    <h6 class="text-uppercase fw-bold mb-4">Contact</h6>
+                    <p><i class="fas fa-home me-3"></i> New York, NY 10012, US</p>
+                    <p>
+                        <i class="fas fa-envelope me-3"></i>
+                        info@example.com
+                    </p>
+                    <p><i class="fas fa-phone me-3"></i> + 01 234 567 88</p>
+                    <p><i class="fas fa-print me-3"></i> + 01 234 567 89</p>
+                </div>
+                <!-- Grid column -->
+            </div>
+            <!-- Grid row -->
+        </div>
+    </section>
+    <!-- Section: Links  -->
+
+    <!-- Copyright -->
+    <div class="text-center p-4 bg-info">
+        © 2023 Company:
+        <a class="text-reset fw-bold" href="#" style="text-decoration: none;">Reminder</a>
+    </div>
+    <!-- Copyright -->
+</footer>
+<!-- Footer -->
