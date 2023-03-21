a87ebe63c73b29698bbdf5d5cbad296050a97eb8
deniskorbakov <korbakovd@gmail.com>
2023-03-21 20:24:11 +0300

    добавил шаблон html

---
+++ resources/views/layouts/html.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>@yield('title')</title>
+    <script src="https://kit.fontawesome.com/bd53d87eef.js" crossorigin="anonymous"></script>
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+</head>
+<body class="d-flex flex-column min-vh-100">
+
+@include('includes.header')
+
+<mai>
+    @yield('main')
+</mai>
+
+@include('includes.footer')
+
+<script src="js/three.r134.min.js"></script>
+<script src="js/vanta.fog.min.js"></script>
+<script>
+    VANTA.FOG({
+        el: "#background-sky",
+        mouseControls: true,
+        touchControls: true,
+        gyroControls: false,
+        minHeight: 200.00,
+        minWidth: 200.00,
+        highlightColor: 0x68b6e1,
+        midtoneColor: 0x52afdc,
+        lowlightColor: 0xd9efa
+    })
+</script>
+</body>
+</html>
