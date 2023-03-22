f59f32a25a435db3bf5cb368d8cdffde0fb44922
deniskorbakov <korbakovd@gmail.com>
2023-03-22 14:34:16 +0300

    добавил ссылки на страницы

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -8,10 +8,10 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Главная</a>
+                    <a class="nav-link active" aria-current="page" href="{{route('welcome')}}">Главная</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Создать напонимание</a>
+                    <a class="nav-link" href="{{route('reminders.index')}}">Создать напонимание</a>
                 </li>
             </ul>
 
