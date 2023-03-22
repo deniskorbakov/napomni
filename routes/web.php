c994a4894e632448a6a9d65a44ec3d2d2ef57f97
deniskorbakov <korbakovd@gmail.com>
2023-03-22 14:35:18 +0300

    сделал группы для записей и изменил главную странциу

--- routes/web.php
+++ routes/web.php
@@ -2,21 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
+Route::get('/', [App\Http\Controllers\WelcomeController::class, 'index'])->name('welcome');
+
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::group(['namespace' => 'App\Http\Controllers\Reminder'],  function() {
+    Route::get('/reminders','IndexController')->name('reminders.index');
+    Route::get('/reminders/create','CreateController')->name('reminders.create');
+});
+
