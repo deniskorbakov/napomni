87c6d427dec792dbfeb765308267b0129b3924b3
deniskorbakov <korbakovd@gmail.com>
2023-03-24 00:09:57 +0300

    добавил столбцов в таблицу

--- database/migrations/2023_03_22_103231_create_recorders_table.php
+++ database/migrations/2023_03_22_103231_create_recorders_table.php
@@ -13,7 +13,9 @@ return new class extends Migration
     {
         Schema::create('recorders', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
             $table->text('content');
+            $table->integer('value');
             $table->foreignId('users_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
