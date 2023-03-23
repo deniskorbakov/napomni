3df6b3d346a947a6fe0d086737c1dd3effea7229
deniskorbakov <korbakovd@gmail.com>
2023-03-23 23:44:43 +0300

    изменил логику вывода для авторизации и регистрации

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg bg-info">
     <div class="container-fluid">
-        <img src="icon/logo.svg" alt="" width="50" height="20">
+        <img src="{{asset('icon/logo.svg')}}" alt="" width="50" height="20">
         <a class="navbar-brand" href="#">Reminder</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -17,17 +17,37 @@
 
             <div class="collapse navbar-collapse " id="navbarNav">
                 <ul class="navbar-nav ms-auto">
-                    @if (Route::has('login'))
-                        @auth
-                            <li class="nav-item"><a class="nav-link active" aria-current="page" href="{{ url('/home') }}">Кабинет пользователя</a></li>
-                        @else
-                            <li class="nav-item"><a class="nav-link active" aria-current="page" href="{{ route('login') }}">Авторизоваться</a></li>
+                    @guest
+                        @if (Route::has('login'))
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">Авторизоваться</a>
+                            </li>
+                        @endif
 
-                            @if (Route::has('register'))
-                                <li class="nav-item"><a class="nav-link active" aria-current="page" href="{{ route('register') }}">Регистрация</a></li>
-                            @endif
-                        @endauth
-                    @endif
+                        @if (Route::has('register'))
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('register') }}">Регистрация</a>
+                            </li>
+                        @endif
+                    @else
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{ Auth::user()->name }}
+                            </a>
+
+                            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                   onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                    Выйти из аккаунта
+                                </a>
+
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
+                    @endguest
                 </ul>
             </div>
         </div>
