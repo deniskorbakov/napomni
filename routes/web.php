33170475bb832161ee331727fabf7cd893cb26dd
deniskorbakov <korbakovd@gmail.com>
2023-03-24 22:25:55 +0300

    добавил доступ страницам авторизованым юзерам

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\Authenticate;
 use Illuminate\Support\Facades\Route;
 
 Auth::routes();
@@ -8,7 +9,7 @@ Route::get('/', [App\Http\Controllers\WelcomeController::class, 'index'])->name(
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::group(['namespace' => 'App\Http\Controllers\Reminder'],  function() {
+Route::middleware('auth')->namespace('App\Http\Controllers\Reminder')->group(function() {
     Route::get('/reminders','IndexController')->name('reminders.index');
     Route::get('/reminders/create','CreateController')->name('reminders.create');
     Route::post('/reminders','StoreController')->name('reminders.store');
