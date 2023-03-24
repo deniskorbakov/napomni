cacbc81e016366104bbeef0f13e45a37d52b031a
deniskorbakov <korbakovd@gmail.com>
2023-03-24 15:02:39 +0300

    сделал правильное наменование столбца

--- database/migrations/2023_03_22_103231_create_recorders_table.php
+++ database/migrations/2023_03_22_103231_create_recorders_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->text('content');
             $table->integer('value');
-            $table->foreignId('users_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
