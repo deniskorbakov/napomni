9063cf626a0b66642d49e00606050b17570f6528
deniskorbakov <korbakovd@gmail.com>
2023-03-24 21:48:36 +0300

    сделал вывод записей по айди юзера

--- app/Http/Controllers/Reminder/IndexController.php
+++ app/Http/Controllers/Reminder/IndexController.php
@@ -3,12 +3,16 @@
 namespace App\Http\Controllers\Reminder;
 
 use App\Http\Controllers\Controller;
+use App\Models\Recorder;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class IndexController extends Controller
 {
     public function __invoke()
     {
-        return(view('reminders.index'));
+        $recorders = Recorder::all()->where('user_id', Auth::id());
+        return (view('reminders.index', compact('recorders')));
+
     }
 }
