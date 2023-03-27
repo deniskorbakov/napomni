698be6ebd4335399899e346c922ba669c52d7d3d
deniskorbakov <korbakovd@gmail.com>
2023-03-27 19:36:22 +0300

    Добавил реквест для обновление записи

---
+++ app/Http/Requests/Reminder/UpdateRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests\Reminder;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|min:3|max:20',
+            'content' => 'required|string|min:3|max:100',
+            'value' => 'required',
+            'date' => 'required',
+            'time' => 'required'
+        ];
+    }
+}
