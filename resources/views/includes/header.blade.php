fc17a6af5a2368efe03abe1c662be104b83f9063
deniskorbakov <korbakovd@gmail.com>
2023-03-28 15:41:25 +0300

    добавил ссылку на переход в кабинет пользователя

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -35,7 +35,14 @@
                                 {{ Auth::user()->name }}
                             </a>
 
+
+
+
                             <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ route('home') }}">
+                                    Кабинет Пользователя
+                                </a>
+
                                 <a class="dropdown-item" href="{{ route('logout') }}"
                                    onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
