a8c47734ec46cc21c7486286ef177b85407a7a2a
deniskorbakov <korbakovd@gmail.com>
2023-03-28 15:41:52 +0300

    добавил аватарку и вывод данных пользователя

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,23 +5,27 @@
 @endsection
 
 @section('main')
-<div class="container mt-5">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Панель управления</div>
+    <div class="container mt-5">
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+        <div class="d-flex">
 
-                    Вы зарегистрированы
-                </div>
+            <!-- Element on the left -->
+            <div class="px-4">
+                <img src="https://ui-avatars.com/api/name={{ Auth::user()->name }}?background=0dcaf0&color=fff" class="rounded-circle" style="width: 150px;"
+                     alt="Avatar">
+
             </div>
+
+            <!-- Vertical divider -->
+            <div class="vr" style="height: 250px;"></div>
+
+            <!-- Element on the left -->
+            <div class="px-4">
+                <h4 class="mt-2 text-gray">Имя пользователя: {{ Auth::user()->name }}</h4>
+                <h4 class="mt-2 text-gray">Почта: {{ Auth::user()->email }}</h4>
+                <h4 class="mt-2 text-gray">Дата создания аккаунта: {{ Auth::user()->created_at }}</h4>
+            </div>
+
         </div>
     </div>
-</div>
 @endsection
