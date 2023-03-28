985fa895a58957682f4a29c68d6ba738f38abc91
deniskorbakov <korbakovd@gmail.com>
2023-03-28 22:49:33 +0300

    подтвердил в роутах верификацию через почту

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,11 @@
 use App\Http\Middleware\Authenticate;
 use Illuminate\Support\Facades\Route;
 
-Auth::routes();
+Auth::routes(['verify' => true]);
 
 Route::get('/', [App\Http\Controllers\WelcomeController::class, 'index'])->name('welcome');
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home')->middleware(["verified"]);
 
 Route::middleware('auth')->namespace('App\Http\Controllers\Reminder')->group(function() {
     Route::get('/reminders','IndexController')->name('reminders.index');
