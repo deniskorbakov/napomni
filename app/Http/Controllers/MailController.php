717e495f99b362e838edd6d9942352e86380a14a
deniskorbakov <korbakovd@gmail.com>
2023-03-29 23:11:48 +0300

    отладка отправки по времене через ссылку

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Mail\SendAllUsers;
+use App\Models\Recorder;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -12,13 +13,18 @@ class MailController extends Controller
 {
     public function send()
     {
-        $users = User::pluck("email");
+//        $usersEmail = User::pluck("email");
+      $today = date("H:i:s");
+      date_default_timezone_set("Europe/Moscow");
+        $recorders = Recorder::all();
+        foreach ($recorders as $recorder) {
+            $recorderTime = $recorder["time"];
+            $recorderTime = substr($recorderTime, 0,-3);
 
-        foreach ($users as $user) {
-            Mail::to($user)->queue(new SendAllUsers());
-
+            if ($recorderTime == date("H:i")) {
+                Mail::to($recorder["user_email"])->send(new SendAllUsers());
+            }
         }
-
 
     }
 }
