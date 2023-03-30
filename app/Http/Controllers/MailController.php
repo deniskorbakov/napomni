adbbef59181e686ac00ed36e8f33eb72be394b63
deniskorbakov <korbakovd@gmail.com>
2023-03-30 14:54:07 +0300

    убрал код

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -13,18 +13,8 @@ class MailController extends Controller
 {
     public function send()
     {
-//        $usersEmail = User::pluck("email");
-      $today = date("H:i:s");
-      date_default_timezone_set("Europe/Moscow");
-        $recorders = Recorder::all();
-        foreach ($recorders as $recorder) {
-            $recorderTime = $recorder["time"];
-            $recorderTime = substr($recorderTime, 0,-3);
-
-            if ($recorderTime == date("H:i")) {
-                Mail::to($recorder["user_email"])->send(new SendAllUsers());
-            }
-        }
+        date_default_timezone_set("Europe/Moscow");
+        echo date("l");
 
     }
 }
