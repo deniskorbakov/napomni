61d560de2b0a993e54db8b832a70bff072205638
deniskorbakov <korbakovd@gmail.com>
2023-03-30 15:53:53 +0300

    исправил ошибку дублирование колонки из бд

--- resources/views/reminders/show.blade.php
+++ resources/views/reminders/show.blade.php
@@ -20,10 +20,10 @@
                 <p class="text-dark mt-2">Название - {{$recorder->name}}</p>
                 <p class="text-dark mt-2">Текст Записи - {{$recorder->content}}</p>
                 <p class="text-dark mt-2">Время записи - {{$recorder->time}}</p>
-                <p class="text-dark mt-2">За сколько присылать уведомление - {{$recorder->value}}</p>
+                <p class="text-dark mt-2">Во сколько присылать уведомления - {{$recorder->notify_for}}</p>
                 <p class="text-dark mt-2">По каким дням присылать уведомление - {{$recorder->date}}</p>
                 <em>Дата создания записи: {{$recorder->created_at}}</em><br>
-                <em>Дата изменения записи: {{$recorder->created_at}}</em><br>
+                <em>Дата изменения записи: {{$recorder->updated_at}}</em><br>
                 <a class="btn btn-info mt-4" href="{{route('reminders.edit', $recorder->id)}}">Изменить</a>
                 <form action="{{route('reminders.delete', $recorder->id)}}" method="POST" class="mt-2">
                     @csrf
