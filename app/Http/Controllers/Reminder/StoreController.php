5cde3d30c835a898c56372c068d93c2931e76f43
deniskorbakov <korbakovd@gmail.com>
2023-03-30 14:54:54 +0300

    изменил логику для коректности данных

--- app/Http/Controllers/Reminder/StoreController.php
+++ app/Http/Controllers/Reminder/StoreController.php
@@ -5,24 +5,36 @@ namespace App\Http\Controllers\Reminder;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Reminder\StoreRequest;
 use App\Models\Recorder;
+use DateTime;
 use Illuminate\Support\Facades\Auth;
 
 class StoreController extends Controller
 {
     public function __invoke(StoreRequest $request)
     {
+        date_default_timezone_set("Europe/Moscow");
         $data = $request->validated();
 
-        Recorder::create([
-            'name' => $data['name'],
-            'content' => $data['content'],
-            'value' => $data['value'],
-            'date' => $data['date'],
-            'time' => $data['time'],
-            'user_id' => Auth::id(),
-            'user_email' => Auth::user()->value('email')
-        ]);
+        //добавляем время
+        $dataTime = new DateTime($data["time"]);
+        //отнимаем от время записи время за сколько уведомлять
+        $dataTime->modify("-${data["value"]} hours");
+        //переводим в формат времени
+        $newTime = $dataTime->format("H:i:s");
 
-        return redirect()->route('reminders.index');
+       Recorder::create([
+           'name' => $data['name'],
+           'content' => $data['content'],
+           'value' => $newTime,
+           'date' => $data['date'],
+           'time' => $data['time'],
+           'user_id' => Auth::id(),
+           'user_email' => Auth::user()->value('email')
+       ]);
+
+       return redirect()->route('reminders.index');
+
+
+
     }
 }
