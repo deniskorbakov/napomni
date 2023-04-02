404a4df3c9d2973208d9f64dbab53ec5e91b70e2
deniskorbakov <korbakovd@gmail.com>
2023-04-02 23:27:58 +0300

    добавил прием двух таблиц

--- app/Http/Controllers/Admin/Reminder/IndexController.php
+++ app/Http/Controllers/Admin/Reminder/IndexController.php
@@ -3,14 +3,17 @@
 namespace App\Http\Controllers\Admin\Reminder;
 
 use App\Http\Controllers\Controller;
+use App\Models\Recorder;
+use App\Models\User;
 
 
 class IndexController extends Controller
 {
     public function __invoke()
     {
-
-        return view('admin.reminders.index');
+        $users = User::all();
+        $recorders = Recorder::all();
+        return view('admin.reminders.index', compact('users', 'recorders'));
 
     }
 }
