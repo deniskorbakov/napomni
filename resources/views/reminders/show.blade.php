3a1db1f80979d158f2abcdf532af47925d0f2f82
deniskorbakov <korbakovd@gmail.com>
2023-04-03 17:14:08 +0300

    вывел переменную для русской даты

--- resources/views/reminders/show.blade.php
+++ resources/views/reminders/show.blade.php
@@ -19,7 +19,7 @@
                 <p class="text-dark mt-2">Текст Записи - {{$recorder->content}}</p>
                 <p class="text-dark mt-2">Время записи - {{$recorder->time}}</p>
                 <p class="text-dark mt-2">Во сколько присылать уведомления - {{$recorder->notify_for}}</p>
-                <p class="text-dark mt-2">По каким дням присылать уведомление - {{$recorder->date}}</p>
+                <p class="text-dark mt-2">По каким дням присылать уведомление - {{$weekRu}}</p>
                 <em>Дата создания записи: {{$recorder->created_at}}</em><br>
                 <em>Дата изменения записи: {{$recorder->updated_at}}</em><br>
                 <a class="btn btn-info mt-4" href="{{route('reminders.edit', $recorder->id)}}">Изменить</a>
