68e04c926c0a589668f16c34ecce20dbb7297e95
deniskorbakov <korbakovd@gmail.com>
2023-04-03 11:52:21 +0300

    вынес логику в сервисы

---
+++ app/Services/Reminder/Service.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Services\Reminder;
+
+use App\Models\Recorder;
+use DateTime;
+use Illuminate\Support\Facades\Auth;
+
+class Service
+{
+    public function store($data) {
+        date_default_timezone_set("Europe/Moscow");
+
+        //добавляем время
+        $dataTime = new DateTime($data["time"]);
+        //отнимаем от время записи время за сколько уведомлять
+        $dataTime->modify("-${data["value"]} hours");
+        //переводим в формат ч:м:с
+        $newTime = $dataTime->format("H:i:s");
+
+        Recorder::create([
+            'name' => $data['name'],
+            'content' => $data['content'],
+            'value' => $data['value'],
+            'notify_for' => $newTime,
+            'date' => $data['date'],
+            'time' => $data['time'],
+            'user_id' => Auth::id(),
+            'user_email' => Auth::user()->value('email')
+        ]);
+    }
+
+    public function update($data, $recorder) {
+        date_default_timezone_set("Europe/Moscow");
+
+        //добавляем время
+        $dataTime = new DateTime($data["time"]);
+        //отнимаем от время записи время за сколько уведомлять
+        $dataTime->modify("-${data["value"]} hours");
+        //переводим в формат времени
+        $newTime = $dataTime->format("H:i:s");
+
+        $recorder->update(
+            [   'name' => $data["name"],
+                'content' => $data["content"],
+                'value' => $data["value"],
+                'notify_for' => $newTime,
+                'date' => $data["date"],
+                'time' => $data["time"],
+            ]
+        );
+    }
+}
