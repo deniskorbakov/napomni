9fd6f7c8762e7e135e22808b29c7814186954980
deniskorbakov <korbakovd@gmail.com>
2023-04-03 17:12:45 +0300

    изменил ввод на русский

--- resources/views/reminders/edit.blade.php
+++ resources/views/reminders/edit.blade.php
@@ -83,14 +83,14 @@
                 </p>
 
                 <select id="inputState" class="form-select" name="date">
-                    <option value="{{$recorder->date}}">{{$recorder->date}}</option>
-                    <option value="Monday">Monday</option>
-                    <option value="Tuesday">Tuesday</option>
-                    <option value="Wednesday">Wednesday</option>
-                    <option value="Thursday">Thursday</option>
-                    <option value="Friday">Friday</option>
-                    <option value="Saturday">Saturday</option>
-                    <option value="Sunday">Sunday</option>
+                    <option value="{{$recorder->date}}">{{$weekRu}}</option>
+                    <option value="Monday">Понедельник</option>
+                    <option value="Tuesday">Вторник</option>
+                    <option value="Wednesday">Среда</option>
+                    <option value="Thursday">Четверг</option>
+                    <option value="Friday">Пятница</option>
+                    <option value="Saturday">Суббота</option>
+                    <option value="Sunday">Воскресенье</option>
                 </select>
             </div>
 
