f96baa30e74ca8d9748f388f4a9f7a83c02dec5b
deniskorbakov <korbakovd@gmail.com>
2023-04-03 17:13:17 +0300

    добавил перевод дня недели

--- app/Http/Controllers/Reminder/EditController.php
+++ app/Http/Controllers/Reminder/EditController.php
@@ -7,6 +7,18 @@ class EditController extends BaseController
 {
     public function __invoke(Recorder $recorder)
     {
-        return view('reminders.edit', compact('recorder'));
+        $dataRu = [
+            'Понедельник'=> 'Monday',
+            'Вторник'=> 'Tuesday',
+            'Среда'=> 'Wednesday',
+            'Четверг' => 'Thursday',
+            'Пятница'=> 'Friday',
+            'Суббота'=> 'Saturday',
+            'Воскресенье'=> 'Sunday',
+        ];
+
+        $weekRu = array_search($recorder['date'], $dataRu);
+
+        return view('reminders.edit', compact('recorder', 'weekRu'));
     }
 }
