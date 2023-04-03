648ac2ccb5b96dd1a0450ee27470db62ad51428c
deniskorbakov <korbakovd@gmail.com>
2023-04-03 22:45:33 +0300

    убрал лишние

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -20,66 +20,7 @@
             </a>
         </div>
     </section>
-    <section>
-        <div class="container text-center text-md-start mt-5">
-            <div class="row mt-3">
-                <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-                    <h6 class="text-uppercase fw-bold mb-4">
-                        <img src="{{asset('icon/logo.svg')}}" alt="logo" width="50" height="20">Reminder
-                    </h6>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias asperiores at atque dolores
-                        doloribus esse incidunt laboriosam minima nam nemo officia officiis omnis sapiente sunt totam,
-                        vitae voluptatem! Doloribus, voluptas.
-                    </p>
-                </div>
-                <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-                    <h6 class="text-uppercase fw-bold mb-4">
-                        Products
-                    </h6>
-                    <p>
-                        <a href="#!" class="text-reset">Angular</a>
-                    </p>
-                    <p>
-                        <a href="#!" class="text-reset">React</a>
-                    </p>
-                    <p>
-                        <a href="#!" class="text-reset">Vue</a>
-                    </p>
-                    <p>
-                        <a href="#!" class="text-reset">Laravel</a>
-                    </p>
-                </div>
-                <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-                    <h6 class="text-uppercase fw-bold mb-4">
-                        Useful links
-                    </h6>
-                    <p>
-                        <a href="#!" class="text-reset">Pricing</a>
-                    </p>
-                    <p>
-                        <a href="#!" class="text-reset">Settings</a>
-                    </p>
-                    <p>
-                        <a href="#!" class="text-reset">Orders</a>
-                    </p>
-                    <p>
-                        <a href="#!" class="text-reset">Help</a>
-                    </p>
-                </div>
-                <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-                    <h6 class="text-uppercase fw-bold mb-4">Contact</h6>
-                    <p><i class="fas fa-home me-3"></i> New York, NY 10012, US</p>
-                    <p>
-                        <i class="fas fa-envelope me-3"></i>
-                        info@example.com
-                    </p>
-                    <p><i class="fas fa-phone me-3"></i> + 01 234 567 88</p>
-                    <p><i class="fas fa-print me-3"></i> + 01 234 567 89</p>
-                </div>
-            </div>
-        </div>
-    </section>
+
     <div class="text-center p-4 bg-info">
         © 2023 Company:
         <a class="text-reset fw-bold" href="#" style="text-decoration: none;">Reminder</a>
