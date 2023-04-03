69358b8510cb418384a59f0732d0aaa71e27840b
deniskorbakov <korbakovd@gmail.com>
2023-04-03 13:28:13 +0300

    переделал вывод записей

--- resources/views/reminders/index.blade.php
+++ resources/views/reminders/index.blade.php
@@ -10,36 +10,27 @@
         <a href="{{route('reminders.create')}}" class="mt-4 btn btn-outline-info">Создать запись</a>
     </div>
 
-    <div class="container mt-5">
-        <div class="row">
-            <div class="col-12 table-responsive">
-                <table class="table table-info table-striped table-hover mt-2">
-                    <thead class="thead">
-                        <th scope="col">Название Записи</th>
-                        <th scope="col">Содержание записи</th>
-                        <th scope="col">Время записи</th>
-                        <th scope="col">Во сколько присылать уведомления</th>
-                        <th scope="col">По каким дням присылать уведомление</th>
-                        <th scope="col">Дата создания записи</th>
-                        <th scope="col">Действие</th>
-                    </thead>
+    <div class="album py-5 bg-body-tertiary">
+        <div class="container">
+            <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+                @foreach($recorders as $record)
+                <div class="col">
+                    <div class="card shadow-sm">
+                        <img src="{{asset('icon/default.svg')}}" class="img-thumbnail">
+                        <div class="card-body">
+                            <p class="card-text">Название: {{ $record->name }}</p>
+                            <p class="card-text">Текст Записи: {{ $record->content }}</p>
+                            <p class="card-text">Время Записи: {{ $record->time}}</p>
+                            <div class="d-flex justify-content-between align-items-center">
 
-                    <tbody>
-                    @foreach($recorders as $record)
-                        <tr>
-                            <td>{{ $record->name }}</td>
-                            <td>{{ $record->content }}</td>
-                            <td>{{ $record->time }}</td>
-                            <td>{{ $record->notify_for}}</td>
-                            <td>{{ $record->date }}</td>
-                            <td>{{ $record->created_at }}</td>
-                            <td>
-                                <a href="{{route('reminders.show', $record->id)}}" class="btn btn-info ms-2"><i class="fa-solid fa-arrow-right"></i></a>
-                            </td>
-                        </tr>
-                    @endforeach
-                    </tbody>
-                </table>
+                                    <a href="{{route('reminders.show', $record->id)}}" class="btn btn-info"><i class="fa-solid fa-arrow-right"></i></a>
+
+                                <small class="text-body-secondary">{{ $record->created_at }}</small>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                @endforeach
             </div>
         </div>
     </div>
