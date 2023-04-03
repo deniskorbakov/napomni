a651ddc9aa09bac5b5463927a79ff04fafbea54f
deniskorbakov <korbakovd@gmail.com>
2023-04-03 11:54:19 +0300

    вынес логику из контроллеров в сервисы для записей

--- app/Http/Controllers/Reminder/StoreController.php
+++ app/Http/Controllers/Reminder/StoreController.php
@@ -2,40 +2,20 @@
 
 namespace App\Http\Controllers\Reminder;
 
-use App\Http\Controllers\Controller;
 use App\Http\Requests\Reminder\StoreRequest;
 use App\Models\Recorder;
 use DateTime;
 use Illuminate\Support\Facades\Auth;
 
-class StoreController extends Controller
+class StoreController extends BaseController
 {
     public function __invoke(StoreRequest $request)
     {
-        date_default_timezone_set("Europe/Moscow");
         $data = $request->validated();
 
-        //добавляем время
-        $dataTime = new DateTime($data["time"]);
-        //отнимаем от время записи время за сколько уведомлять
-        $dataTime->modify("-${data["value"]} hours");
-        //переводим в формат времени
-        $newTime = $dataTime->format("H:i:s");
+        $this->service->store($data);
 
-       Recorder::create([
-           'name' => $data['name'],
-           'content' => $data['content'],
-           'value' => $data['value'],
-           'notify_for' => $newTime,
-           'date' => $data['date'],
-           'time' => $data['time'],
-           'user_id' => Auth::id(),
-           'user_email' => Auth::user()->value('email')
-       ]);
-
-       return redirect()->route('reminders.index');
-
-
+        return redirect()->route('reminders.index');
 
     }
 }
--- app/Http/Controllers/Reminder/UpdateController.php
+++ app/Http/Controllers/Reminder/UpdateController.php
@@ -2,36 +2,17 @@
 
 namespace App\Http\Controllers\Reminder;
 
-use App\Http\Controllers\Controller;
-use App\Http\Requests\Reminder\StoreRequest;
 use App\Http\Requests\Reminder\UpdateRequest;
 use App\Models\Recorder;
 use DateTime;
-use Illuminate\Support\Facades\Auth;
-
-class UpdateController extends Controller
+class UpdateController extends BaseController
 {
     public function __invoke(UpdateRequest $request, Recorder $recorder)
     {
-        date_default_timezone_set("Europe/Moscow");
         $data = $request->validated();
 
-        //добавляем время
-        $dataTime = new DateTime($data["time"]);
-        //отнимаем от время записи время за сколько уведомлять
-        $dataTime->modify("-${data["value"]} hours");
-        //переводим в формат времени
-        $newTime = $dataTime->format("H:i:s");
+        $this->service->update($data, $recorder);
 
-        $recorder->update(
-            [   'name' => $data["name"],
-                'content' => $data["content"],
-                'value' => $data["value"],
-                'notify_for' => $newTime,
-                'date' => $data["date"],
-                'time' => $data["time"],
-            ]
-        );
         return redirect()->route('reminders.show',$recorder->id);
     }
 }
