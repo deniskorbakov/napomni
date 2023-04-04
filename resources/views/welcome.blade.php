850fcf90e3e68ff44789542544824dc676011dde
deniskorbakov <korbakovd@gmail.com>
2023-04-04 21:13:42 +0300

    подправил отображение заднего фона облаков

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,16 +5,14 @@
 @endsection
 
 @section('main')
-    <div class="position-relative overflow-hidden p-3 p-md-5 text-center bg-info border-top border-dark"
+    <div class="position-relative overflow-hidden  text-center bg-info border-top border-dark"
          id="background-sky">
-        <div class="col-md-5 p-lg-5 mx-auto my-5 tracking-in-expand">
+        <div class="col-md-5 p-lg-5 mx-auto my-5">
             <h1 class="display-4 fw-normal">Добро Пожаловать</h1>
             <p class="lead fw-normal">
                 Не упустите ни одной важной записи - получайте уведомления на почту!
             </p>
         </div>
-        <div class="product-device shadow-sm d-none d-md-block"></div>
-        <div class="product-device product-device-2 shadow-sm d-none d-md-block"></div>
     </div>
 
     <div class="px-4 py-5 my-5 text-center">
