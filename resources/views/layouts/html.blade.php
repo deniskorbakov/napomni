f51656842f855c0241d56da4891ec11edfe87356
deniskorbakov <korbakovd@gmail.com>
2023-04-04 21:12:41 +0300

    перешел на cdn

--- resources/views/layouts/html.blade.php
+++ resources/views/layouts/html.blade.php
@@ -8,7 +8,7 @@
     <script src="https://kit.fontawesome.com/bd53d87eef.js" crossorigin="anonymous"></script>
     <link rel="icon" href="icon/logo.svg">
     <link rel="stylesheet" href="css/style.css">
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 </head>
 <body class="d-flex flex-column min-vh-100">
 
